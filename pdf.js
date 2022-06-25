f06a5db9b0117c8fc552a2134026fc2de0582aab
saranvlmna <saranvlmna@gmail.com>
2022-06-25 20:06:44 +0530

    update

--- pdf.js
+++ pdf.js
@@ -3,6 +3,7 @@ const path = require('path')
 const utils = require('util')
 const puppeteer = require('puppeteer')
 const hb = require('handlebars')
+const { resolve } = require('path')
 const readFile = utils.promisify(fs.readFile)
 async function getTemplateHtml() {
     console.log("Loading template file in memory")
@@ -13,25 +14,43 @@ async function getTemplateHtml() {
         return Promise.reject("Could not load html template");
     }
 }
-async function generatePdf() {
+ function generatePdf() {
     let data = {};
     getTemplateHtml().then(async (res) => {
-        console.log("Compiing the template with handlebars")
-        const template = hb.compile(res, { strict: true });
-        const result = template(data);
-        const html = result;
-        const browser = await puppeteer.launch(
-            {
-                executablePath: '/usr/bin/chromium-browser'
-            }
-        );
-        const page = await browser.newPage()
-        await page.setContent(html)
-        await page.pdf({ path: 'invoice.pdf', format: 'A4' })
-        await browser.close();
-        console.log("PDF Generated")
+        return new Promise(async(resolve) => {
+            console.log("Compiing the template with handlebars")
+            const template = hb.compile(res, { strict: true });
+            const result = template(data);
+            const html = result;
+            const browser = await puppeteer.launch(
+                {
+                    executablePath: '/usr/bin/chromium-browser'
+                }
+            );
+            const page = await browser.newPage()
+            await page.setContent(html)
+            await page.pdf({
+                margin: {
+                    format: 'A4',
+                    'left': 90,
+                    'top': 30,
+                    'bottom': 30
+                }
+    
+            }).then((res) => {
+                const string = res.toString('base64');
+                resolve(string)
+                console.log(string)
+            })
+            await browser.close();
+            
+        })
     }).catch(err => {
         console.error(err)
     });
 }
+
+
+
+
 generatePdf();
